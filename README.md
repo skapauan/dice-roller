6fbe4c823bcdcbb783efa5142b5299f7f729521c
skapauan <skapauan@users.noreply.github.com>
2021-01-14 14:22:40 -0600

    Manage config with dotenv

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # dice-roller
 App to simulate dice rolls and save your common rolls. Uses true random with fallback to pseudorandom.
+
+## Setup
+
+### Environment Variables
+`PORT` - Port to run the server on.
+`RANDOMORG_API_KEY` - API key for Random.org.
--- index.js
+++ index.js
@@ -1,11 +1,17 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
+
 const http = require('http')
-const port = process.env.PORT || 3000
 
 const server = http.createServer((req, res) => {
     res.statusCode = 200
     res.end('<html><head><title>Dice Roller</title></head><body><p>Dice Roller is under construction!</body></html>')
 })
 
+const port = process.env.PORT || 3000
+const hasRandomKey = process.env.RANDOMORG_API_KEY ? true : false;
+
 server.listen(port, () => {
-    console.log(`Server running on port ${port}`)
+    console.log(`Server running on port ${port}. Random.org API key ${hasRandomKey ? 'found' : 'not found'}.`)
 })

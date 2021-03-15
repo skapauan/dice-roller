aa88397d1941970746c0acc2c6bfac184d61df53
skapauan <skapauan@users.noreply.github.com>
2021-03-15 18:12:42 -0500

    Initialize database before starting server

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,17 @@
 import server from './server'
+import db from './db/db'
 
 if (process.env.NODE_ENV !== 'production') require('dotenv').config()
 
 const port = process.env.PORT || 3000
 const hasRandomKey = process.env.RANDOMORG_API_KEY ? true : false;
 
-server.listen(port, () => {
-    console.log(`Server running on port ${port}. Random.org API key ${hasRandomKey ? 'found' : 'not found'}.`)
+db.init()
+.then(() => {
+    server.listen(port, () => {
+        console.log(`Server running on port ${port}. Random.org API key ${hasRandomKey ? 'found' : 'not found'}.`)
+    })
+})
+.catch((error) => {
+    console.log(`Server not started, could not init database. ${error}`)
 })

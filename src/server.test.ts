5af5f5d601e02b09503a72d70a3a9d9fc4621a93
skapauan <skapauan@users.noreply.github.com>
2021-04-21 11:17:14 -0500

    Server has login and password services

--- src/server.test.ts
+++ src/server.test.ts
@@ -3,24 +3,63 @@ import path from 'path'
 import getServer from './server'
 import DB from './db/db'
 import { testConfig, getTestSchema } from './db/testconfig'
+import PwtokensTable from './db/pwtokens'
 
 const schema = getTestSchema()
 const db = new DB(testConfig, schema)
 const server = getServer(db, path.join(__dirname, '..', 'public'), process.env)
+const pwtokensTable = new PwtokensTable(db)
 
 beforeAll(async () => {
     await db.init()
+    await pwtokensTable.deleteAll()
 })
 
 afterAll(() => {
     db.end()
 })
 
-describe('Home page', () => {
-    it('should render', async () => {
-        await request(server)
-            .get('/')
-            .expect(200)
-            .expect(/\bDice Roller\b/)
+describe('Server', () => {
+
+    describe('Home page', () => {
+        it('should be reachable', async () => {
+            await request(server)
+                .get('/')
+                .expect(200)
+                .expect(/\bDice Roller\b/)
+        })
     })
+    
+    describe('Login service', () => {
+        it('should be reachable', () => {
+            return request(server)
+                .post('/login')
+                .type('application/json')
+                .send({ user: 'mystery@example.com', password: null })
+                .expect(400)
+                .expect('Content-Type', /json/)
+                .then((res) => {
+                    expect(res.body).toHaveProperty('success', false)
+                })
+        })
+    })
+    
+    describe('Password service', () => {
+        it('should be reachable', () => {
+            return request(server)
+                .post('/password')
+                .type('application/json')
+                .send({
+                    token: 'mystery token',
+                    user: 'mystery@example.com',
+                    newPassword: 'mystery password'
+                })
+                .expect(403)
+                .expect('Content-Type', /json/)
+                .then((res) => {
+                    expect(res.body).toHaveProperty('success', false)
+                })
+        })
+    })
+    
 })
